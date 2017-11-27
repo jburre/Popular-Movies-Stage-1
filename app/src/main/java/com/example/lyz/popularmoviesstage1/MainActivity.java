a893a3b8f3f62bffc4f109967da067a4b5d1808c
Jan-Henrik <Jan.H.Burre@gmail.com>
2017-11-27 18:28:24 +0100

    style styled the main activity and detail
    also made a small test if the corresponding view works as expected

--- app/src/main/java/com/example/lyz/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/lyz/popularmoviesstage1/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.lyz.popularmoviesstage1;
 
+import android.content.Context;
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView mGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGridView = findViewById(R.id.gv_main);
+        /* TEST for appliance TODO remove later
+        String[] myArray = new String[100];
+        for (int i=0;i<myArray.length;i++){
+            myArray[i]=String.valueOf(i);
+        }
+        ArrayAdapter<String> mListAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,myArray);
+        mGridView.setAdapter(mListAdapter);
+        */
     }
 }
